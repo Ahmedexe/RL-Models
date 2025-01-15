9be7fadf29ed2b0f04d2dbf0570be93087bdf93f
Ahmed <alzuhairahmed@gmail.com>
2025-01-15 23:08:02 +0300

    apply model

--- tictactoe/env.py
+++ tictactoe/env.py
@@ -3,52 +3,56 @@ import copy
 
 class TicTacToe:
     def __init__(self):
-        self.state = ["0","0","0","0","0","0","0","0","0"]
+        self.state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    def play(self, state, action, player):
-        prevState = copy.deepcopy(state)
-        if state[action] == "0":
-            state[action] = player
-        reward = rewardCalc(prevState, state, action)
-        done = isWon(state)
+    def play(self, action, player):
+        prevState = copy.deepcopy(self.state)
+        if self.state[action] == 0:
+            self.state[action] = player
+        reward = rewardCalc(prevState, self.state, action)
+        done = isWon(self.state)
 
-        return state, reward, done
-    
+        next = copy.deepcopy(self.state)
+
+        return prevState, next, reward, done
+
     def reset(self):
-        self.state = ["0","0","0","0","0","0","0","0","0"]
+        self.state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         return self.state
 
-
-
-
+    def print(self):
+        for i in range(3):
+            for j in range(3):
+                print(self.state[j + 3*i])
+            print()
 
 
 def rewardCalc(prevState, nextState, action):
 
-    if prevState[action] != "0":
+    if prevState[action] != 0:
         reward = -10
     elif isWon(nextState):
-        reward = 10 
+        reward = 10
     else:
-        reward = 1
+        reward = 0
 
     return reward
+
 
 def isWon(state):
 
     # check rows
     for i in range(3):
-        if state[0 + 3*i] == state[1 + 3*i] == state[2 + 3*i] != "0":
+        if state[0 + 3*i] == state[1 + 3*i] == state[2 + 3*i] != 0:
             return 1
-        
-        if state[0 + i] == state[3 + i] == state[6 + i] != "0":
+
+        if state[0 + i] == state[3 + i] == state[6 + i] != 0:
             return 1
-        
-    if state[0] == state[4] == state[8] != "0":
+
+    if state[0] == state[4] == state[8] != 0:
         return 1
-    
-    if state[2] == state[4] == state[6] != "0": 
+
+    if state[2] == state[4] == state[6] != 0:
         return 1
 
     return 0
- 
--- tictactoe/tictactoeM.py
+++ tictactoe/tictactoeM.py
@@ -2,20 +2,22 @@ import torch.nn as nn
 import torch
 import random
 import numpy as np
-import torch.optim as  optim
+import torch.optim as optim
 from env import TicTacToe
+import copy
 
 
-stateSize = 3**9
+stateSize = 9
 actionSize = 9
-bufferSize = 8000
-batchSize = 32
+bufferSize = 2000
+batchSize = 64
 gamma = 0.95
 epsilon = 1.
-epsilonDecay = 0.995
-alpha = 0.003
-episodes = 350
-targetupdate = 128
+epsilonDecay = 0.875
+alpha = 0.001
+episodes = 10000
+targetupdate = 256
+experiences = []
 
 
 class QNN(nn.Module):
@@ -29,7 +31,6 @@ class QNN(nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         return self.fc3(x)
-
 
 
 env = TicTacToe()
@@ -52,15 +53,60 @@ for episode in range(episodes):
     totalReward = 0
     done = False
 
+    if len(experiences) > bufferSize:
+        experiences.pop(0)
+
     while not done:
 
         # Epsilon greedy Exploration..
         if random.random() < epsilon:
-            action = str(random.randint(0, 8))
+            random.seed(0)
+            action = np.random.randint(8)
 
         else:
-            stateTensor = torch.tensor([float(x) for x in state])
-            action = torch.argmax(policyNN(stateTensor)).item()
+            stateTensor = torch.tensor(state, dtype=torch.float32)
+            with torch.no_grad():
+                action = int(torch.argmax(policyNN(stateTensor)).item())
 
+        state, nextState, reward, done = env.play(action, 1)
+        experiences.append((state, action, reward, nextState, done))
+        state = copy.deepcopy(nextState)
+        totalReward += reward
 
-        ## Take Action ----> To Be Conti.
+        # Second player plays
+        with torch.no_grad():
+            stateTensor = torch.tensor(state, dtype=torch.float32)
+            random.seed(42)
+            partialReward = 0
+            # while partialReward < 1:
+            #     _, state, partialReward, done = env.play(
+            #         random.randint(0, 8), 2)
+            _, state, _, done = env.play(random.randint(0, 8), 2)
+
+        if len(experiences) >= batchSize:
+            expSample = random.sample(experiences, batchSize)
+            stateSample, actionSample, rewardSample, nextStateSample, doneSample = zip(
+                *expSample)
+
+            stateSample = torch.tensor(stateSample, dtype=torch.float32)
+            actionSample = torch.tensor(actionSample, dtype=torch.int64)
+            rewardSample = torch.tensor(rewardSample, dtype=torch.float32)
+            nextStateSample = torch.tensor(
+                nextStateSample, dtype=torch.float32)
+            doneSample = torch.tensor(doneSample, dtype=torch.float32)
+
+            Qvals = policyNN(stateSample).gather(
+                1, actionSample.unsqueeze(1)).squeeze(1)
+
+            nextQvals = targetNN(nextStateSample).max(1)[0].detach()
+            targetQs = rewardSample + (gamma * nextQvals * (1-doneSample))
+
+            # Update QNN params
+            loss = nn.MSELoss()(Qvals, targetQs)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        epsilon = max(0.1, epsilonDecay * epsilon)
+
+        print(f"Episode {episode + 1}: Total Reward = {totalReward}")
